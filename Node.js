9b59f12784c94ebee34e95ee9c260eaa1aa49e4e
Carlos Eduardo Rebelato <Carlos Eduardo Rebelato>
2016-05-03 18:56:02 -0300

    node builder

---
+++ Node.js
@@ -0,0 +1,15 @@
+var reach [];
+int number;
+boolean isFirstNode;
+boolean isLastNode;
+
+function Node(reach, number, isFirstNode, isLastNode){
+	this.reach = reach;
+	this.number = number;
+	this.isFirstNode = isFirstNode;
+	this.isLastNode = isLastNode;
+}
+
+Node.prototype = {
+	constructor: Node
+};
--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-function print(){
-    
-    var testString = $('#txt1').val() 
-    
-    var lines = testString.split("(\n|\r)");
-   
-    for (var i = 0; i< testString.length; i++){
-        console.log(lines[i] + +"  -------  " + i);
-    }
-    
- console.log($('#txt1').val());
+var Node = function (reach, number, isFirstNode, isLastNode){
+	this.reach = reach;
+	this.number = number;
+	this.isFirstNode = isFirstNode;
+	this.isLastNode = isLastNode;
+}
+
+function runSubmit(){
+	var x = initNodes();
+}
+
+function initNodes(){
+	var x = createNodes();
+	setStartingNodes(x);
+	setEndingNodes(x);
+	setReach(x);
+	x.forEach(function(entry){
+		console.log(entry);
+	});
+	return x;
+}
+
+function createNodes(){
+	var list = [];
+	$("#nodesTxt").val().split(",").forEach(function(entry){
+		list[entry] = new Node([], entry, false, false);
+	});
+	return list;
+}
+
+function setStartingNodes(list){
+	$("#startTxt").val().split(",").forEach(function(entry){
+		list[entry].isFirstNode = true;
+	});
+}
+
+function setEndingNodes(list){
+	$("#endTxt").val().split(",").forEach(function(entry){
+		list[entry].isLastNode = true;
+	});
+}
+
+function setReach(list){
+	$("#edgeTxt").val().split(";").forEach(function(entry){
+		var nodes = entry.split(",");
+		list[nodes[0]].reach.push(list[nodes[1]]);
+	});
 }
